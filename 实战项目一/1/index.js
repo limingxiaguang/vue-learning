3ef78d0c9aa4d026f49232cac142bd689323dd83
佳明 <1635739547@qq.com>
2019-04-23 19:35:24 +0800

    给项目实战一中第一个项目增加注释

--- 实战项目一/1/index.js
+++ 实战项目一/1/index.js
@@ -25,24 +25,31 @@ var vm = new Vue({
         }
     },
     computed: {
+        // 计算总的价格
         totalPrice:function() {
             var total = 0
+            // 循环遍历出list里面的所有商品
             for(var i = 0; i < this.list.length; i++){
                 var item = this.list[i]
                 total += item.price * item.count
             }
+            // 价格每三位用 ',' 隔开
             return total.toString().replace(/\B(?=(\d{3})+$)/g,',')
         }
     },
     methods: {
         handelReduce(index){
+            // 判断该商品的数量是否为1，为一就return掉
             if(this.list[index].count === 1) return;
+            // 数量减一
             this.list[index].count--;
         },
         handelAdd(index){
+            // 数量加1
             this.list[index].count++;
         },
         handelRemove(index){
+            // 删除list数组中该下标的元素
             this.list.splice(index, 1)
         }
     }
